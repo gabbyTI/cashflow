d9c26b39fb7529ad9a71dca32e42e8799a319061
Gabriel <gabrielibenye@gmail.com>
2018-08-26 05:13:24 +0100

    Changed the html pages to php and added the header and footer php include file, i editted the links href also

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+    include 'includes/header.php';
+?>
+        <br><br><br>
+        <!--Main Body Starts-->
+        <div class="container">
+            <div class="card" style="padding: 30px">
+                <b>Cashflow - SIGN IN</b>
+                <hr>
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Username*</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username" required>
+                    </div>
+                
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </form>
+            </div>
+        </div>
+        <!--Mian body ends-->
+
+        <hr>
+<?php
+    include 'includes/footer.php';
+?>
