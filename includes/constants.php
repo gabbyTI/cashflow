4a7d6259226ea79fd305444edb2d8d6b789b5493
Bien Nwinate <42247370+BienGlitch@users.noreply.github.com>
2018-08-27 13:02:34 +0100

    Add files via upload

--- includes/constants.php
+++ includes/constants.php
@@ -1,8 +1,7 @@
 <?php 
     // DB constants
-
     define("DB_NAME","cashflow");
     define("DB_USER","root");
-    define("DB_PASS","");
+    define("DB_PASS","59crumuagholu");
     define("DB_SERVER","localhost");
 ?>
