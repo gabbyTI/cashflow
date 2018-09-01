9e1f470788bd698cf2488c40d5a7d438b560fb86
Victor <uchechukwueze70@gmail.com>
2018-09-01 14:30:21 +0100

    .

--- includes/constants.php
+++ includes/constants.php
@@ -1,18 +1,8 @@
 <?php 
     // DB constants
 
-<<<<<<< HEAD
     define("DB_NAME","cashflow");
     define("DB_USER","root");
     define("DB_PASS","");
     define("DB_SERVER","localhost");
 ?>
-=======
-    define("DB_NAME", "cashflow");
-    define("DB_USER", "root");
-    define("DB_PASS", "cash1234flow");
-    define("DB_SERVER", "localhost");
-
-    ?>
-
->>>>>>> b171a0eaa9ebce6c4a21f07cf98b770a1e59b566
